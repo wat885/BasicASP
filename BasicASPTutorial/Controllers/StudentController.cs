5921ac294fc8af424a8ea02ea91cfbbbad1e5efb
watcharinta <watcharin.ta@os.ntplc.co.th>
2023-10-31 10:12:04 +0700

    create Controller

---
+++ BasicASPTutorial/Controllers/StudentController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BasicASPTutorial.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("คะแนนสอบวิชาคอม");
+        }
+        public IActionResult Details()
+        {
+            return Content("ราลละเอียดคะแนนสอบ");
+        }
+        public IActionResult ShowScore(int id) {
+            return Content($"คะแนนสอบของนักเรียน เลขที่ {id}");
+        }
+    }
+}
